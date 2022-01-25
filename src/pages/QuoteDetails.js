95b3b807fc6c6c34e30edb75f4c426544ef89472
mohsenrs <mohsen8022@gmail.com>
2022-01-25 09:58:08 +0330

    layout

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Route, useParams, Link } from 'react-router-dom'
+import Comments from './../components/comments/Comments'
 
 function QuoteDetails() {
-  return <h1>quote details</h1>
+  const params = useParams()
+
+  return (
+    <>
+      <h1>quote details</h1>
+      <p>{params.quoteId}</p>
+      <Link to={`/quotes/${params.quoteId}/comments`}>Comments</Link>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  )
 }
 
 export default QuoteDetails
